dbe662d6d1e27be7047dd72e73f1036304729f9d
AustinH1131 <athleticatxbox2@gmail.com>
2021-10-15 12:53:25 -0700

    Single player and multiplayer choice

--- main .py
+++ main .py
@@ -1,2 +1,25 @@
 print("Welcome to Rock Paper Scissors Lizard Spock!")
-print("Heres how to play:")
+print(" ")
+print("Heres how to play: ")
+print("Rock crushes Scissors, Rock crushes Lizard")
+print("Paper covers rock, Paper disproves Spock")
+print("Scissors cuts Paper, Scissors decapitates Lizard")
+print("Lizard poisons Spock, Lizard eats Paper")
+print("Spock vaporizes Rock, Spock smashes Scissors")
+print(" ")
+print("Winner will be determined by a best of 3.")
+
+print("Choose Single player or Multiplayer")
+singlep=input(" ").upper
+if singlep=="SINGLE PLAYER":
+     pass
+multi=input(" ").upper
+if multi=="MULTIPLAYER":
+    pass
+
+
+
+
+
+
+
